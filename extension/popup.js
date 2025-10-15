ec3867b0763401ea15fc350db315e2abf2bb6b03
t-ski <t.schiepanski@gmx.de>
2025-10-15 12:38:25 +0200

    Style popup

--- extension/popup.js
+++ extension/popup.js
@@ -1,49 +1,48 @@
+browser.browserAction
+    .setPopupStyles({
+        backgroundColor: "transparent"
+    });
+browser.browserAction.resizePopup(450, 730);
+browser.browserAction.detachPopup();
+browser.browserAction.openPopup();
+
+
 const CLIENT_TOOLS = {
-    // Note: To use this example, the client tool called "redirectToExternalURL" (case-sensitive) must have been created with the configuration defined above.
-    redirectToURL({ url }) {
-        console.debug('redirecting to: ', url);
-
-        browser.webfuseSession.relocate(url);
-        //window.open(url, '_blank', 'noopener,noreferrer');
-    },
-
-    async takeDomSnapshot() {
+    async take_dom_snapshot() {
         return browser.webfuseSession
             .automation
             .take_dom_snapshot({
-                modifier: 'downsample',
+                modifier: "downsample",
             });
     },
 
-    async leftClick(selector) {
+    async left_click({ selector }) {
         return browser.webfuseSession
             .automation
             .left_click(selector);
     },
 
-    async mouseMove(x, y) {
-        return browser.webfuseSession
-            .automation
-            .mouse_move([x, y]);
-    },
-
-    async type(text, selector) {
+    async type({ text, selector }) {
         return browser.webfuseSession
             .automation
             .type(text, selector);
+    },
+
+    relocate({ url }) {
+        browser.webfuseSession.relocate(url);
     }
 };
 
 
 function init() {
-    const widget = document.querySelector('elevenlabs-convai');
+    const widget = document.querySelector("elevenlabs-convai");
 
-    // Listen for the widget's 'call' event to trigger client-side tools
+    // Listen for the widget"s "call" event to trigger client-side tools
     widget
-        ?.addEventListener('elevenlabs-convai:call', (event) => {
+        ?.addEventListener("elevenlabs-convai:call", (event) => {
             event.detail.config.clientTools = CLIENT_TOOLS;
         });
 }
 
 
-document.addEventListener('DOMContentLoaded', init);
+document.addEventListener("DOMContentLoaded", init);
